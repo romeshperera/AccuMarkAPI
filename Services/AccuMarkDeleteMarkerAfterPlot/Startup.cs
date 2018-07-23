d05ccc7fb10f72138bef1ee224a02a2e12f0ca2d
vp.romesh.perera <romeshperera@virtusa.com>
2018-07-23 02:19:25 -0400

    Service name changed

--- Services/AccuMarkDeleteMarkerAfterPlot/Startup.cs
+++ Services/AccuMarkDeleteMarkerAfterPlot/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace AccuMarkDeleteMarkerAfterExport
+namespace AccuMarkDeleteMarkerAfterPlot
 {
     public class Startup
     {
